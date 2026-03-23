17055b33792f2b71a90f60f27c070f8058db0ae3
Marvin <johnmarvindigal@gmail.com>
2026-03-23 16:06:42 +0400

    Restructure: move frontend files into /frontend folder

--- asset-buddy/backend/models/Department.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const departmentSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    location: { type: String, default: '' },
-    pcCount: { type: Number, default: 0 },
-    printerCount: { type: Number, default: 0 },
-}, { timestamps: true });
-module.exports = mongoose.model('Department', departmentSchema);
